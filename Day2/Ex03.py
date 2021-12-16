6bdd2ac906c2ab28efbc991215a7f050ccf7a3ef
Prashant <95084475+Prashantty@users.noreply.github.com>
2021-12-16 23:30:09 +0530

    Create Ex03.py

---
+++ Day2/Ex03.py
@@ -0,0 +1,16 @@
+# methematical calculation and precedence of the operators
+y = (3 + 5) * 6
+x = 3 + 5 * 6
+
+a = 3+5*2**2/16-1
+
+b=((3+5)*2)**2/16-1
+
+c=5**3
+
+
+print(x)
+print(y)
+print(a)
+print(b)
+print(c)
