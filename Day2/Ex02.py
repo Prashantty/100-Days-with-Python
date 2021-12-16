9a2f3f502b471c601f86e235c71c420894b31f15
Prashant <95084475+Prashantty@users.noreply.github.com>
2021-12-16 23:22:54 +0530

    Create Ex02.py

---
+++ Day2/Ex02.py
@@ -0,0 +1,20 @@
+#type casting 
+
+name=str(len(input("what is your name\n")))
+print("your name has " + name +" latters")
+a=123
+print(type(a))
+print(a +float(12.5))
+
+digit=input("Enter two digit number::")
+first=int(digit[0])
+second=int(digit[1])
+print("your first digit is ::"first)
+print("our second digit is:::"second)
+print(first+second)
+
+# or
+
+digit=input("Enter two digit number::")
+addition =int(digit[0])+int(digit[1])
+print(addition)
