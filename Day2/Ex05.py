2e69a32744e993384d6a761dfdfb62a2aa983cf2
Prashant <95084475+Prashantty@users.noreply.github.com>
2021-12-17 15:02:19 +0530

    Create Ex05.py

---
+++ Day2/Ex05.py
@@ -0,0 +1,9 @@
+#sttring manupulation
+
+score=20
+height=5.7
+Gu=True
+print("his score is "+str(score)+ " is height is "+str(height))
+print(" Are you in galgotias university"+str(Gu))
+
+print(f"His score is {score}, His height is {height}, and he is from galgotias university {Gu} ")
