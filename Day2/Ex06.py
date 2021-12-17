cbee81882cd47981d3c12357e6d576e98858eb25
Prashant <95084475+Prashantty@users.noreply.github.com>
2021-12-17 15:13:45 +0530

    Create Ex06.py

---
+++ Day2/Ex06.py
@@ -0,0 +1,10 @@
+#  calculator
+
+age=int(input("Enter your age "))
+
+year=90-age
+days=year*365
+month=year*12
+weeks=year*52
+
+print(f"you have {year} year, {month} month, {weeks} weeks , and {days} days left in your life ")
