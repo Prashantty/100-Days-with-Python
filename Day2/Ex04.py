60adcba8f00b3dc0481f9e16355365e8a64ac505
Prashant <95084475+Prashantty@users.noreply.github.com>
2021-12-17 15:01:14 +0530

    Create Ex04.py

---
+++ Day2/Ex04.py
@@ -0,0 +1,15 @@
+#bmi calculator
+
+height=input("Enter the height")
+weight=input("Enter the weight")
+
+bmi=int(height)/float(weight)**2
+bmi1=int(bmi)
+print("our bmi calculator is :::")
+print(bmi1)
+
+#round function in python
+print(round(8/3,3))
+print(int(11/3))
+print(type(11//3))
+print(type(11/3))
