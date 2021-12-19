31ce32099e6b4b8bce560d2c3a0b3244dc8d0649
Prashant <95084475+Prashantty@users.noreply.github.com>
2021-12-20 00:35:34 +0530

    Create Ex02.py

---
+++ Day5/Ex02.py
@@ -0,0 +1,13 @@
+#for loop day05 and Ex01
+
+friends=["Aman","Prabhat","Prashant","Pranav" ]
+for friend in friends :
+    print(friend + " Kumar")
+
+for loop Day05 and Ex02
+
+height = input("Enter some different Heights!!\n").split()
+for n in range(0, len(height)):
+    height[n]=int(height[n])
+print(height) 
+length_height=len(height)
