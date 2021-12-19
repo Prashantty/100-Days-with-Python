e2681320513b1b2f128828f5d5ed291886416da2
Prashant <95084475+Prashantty@users.noreply.github.com>
2021-12-20 00:36:29 +0530

    Update Ex02.py

--- Day5/Ex02.py
+++ Day5/Ex02.py
@@ -4,10 +4,3 @@ friends=["Aman","Prabhat","Prashant","Pranav" ]
 for friend in friends :
     print(friend + " Kumar")
 
-for loop Day05 and Ex02
-
-height = input("Enter some different Heights!!\n").split()
-for n in range(0, len(height)):
-    height[n]=int(height[n])
-print(height) 
-length_height=len(height)
